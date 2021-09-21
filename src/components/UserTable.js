d4f316a3b9cd205002e5f754ab6f64cdd4fdad57
om1621 <onkartelange2000@gmail.com>
2021-09-21 19:08:14 +0530

    fixed bug

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -6,22 +6,28 @@ import axios from "axios";
 const UserTable = () => {
   const [userTableData, setUserTableData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [updating, setUpdaing] = useState(false);
 
   const addUser = async (newData) => {
+    setUpdaing(true);
     const { data } = await axios.post(
       `https://jsonplaceholder.typicode.com/users`,
       newData
     );
     setUserTableData([...userTableData, data]);
+    setUpdaing(false);
   };
 
   const removeUser = async (id) => {
+    setUpdaing(true);
     await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
     const updatedTableData = userTableData.filter((user) => user.id !== id);
     setUserTableData(updatedTableData);
+    setUpdaing(false);
   };
 
   const updateUser = async (id, newData) => {
+    setUpdaing(true);
     const { data } = await axios.put(
       `https://jsonplaceholder.typicode.com/users/${id}`,
       newData
@@ -35,6 +41,7 @@ const UserTable = () => {
     });
 
     setUserTableData(updatedUserList);
+    setUpdaing(false);
   };
 
   useEffect(() => {
@@ -55,46 +62,53 @@ const UserTable = () => {
       {loading ? (
         <h2>Loading.....</h2>
       ) : (
-        <MaterialTable
-          title="User Table"
-          columns={userTableColumns}
-          data={userTableData}
-          editable={{
-            onRowAdd: (newData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  addUser(newData);
-                  resolve();
-                }, 1000);
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  updateUser(oldData.id, newData);
-                  resolve();
-                }, 1000);
-              }),
-            onRowDelete: (oldData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  removeUser(oldData.id);
-                  resolve();
-                }, 1000);
-              }),
-          }}
-          cellEditable={{
-            onCellEditApproved: (newValue, oldValue, rowData, columnDef) => {
-              return new Promise((resolve, reject) => {
-                setTimeout(() => {
+        <React.Fragment>
+          <MaterialTable
+            title="User Table"
+            columns={userTableColumns}
+            data={userTableData}
+            editable={{
+              onRowAdd: (newData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    addUser(newData);
+                    resolve();
+                  }, 1000);
+                }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    updateUser(oldData.id, newData);
+                    resolve();
+                  }, 1000);
+                }),
+              onRowDelete: (oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    removeUser(oldData.id);
+                    resolve();
+                  }, 1000);
+                }),
+            }}
+            cellEditable={{
+              onCellEditApproved: (newValue, oldValue, rowData, columnDef) => {
+                return new Promise(async (resolve, reject) => {
                   const newData = rowData;
                   newData[columnDef.field] = newValue;
-                  updateUser(rowData.id, newData);
-                  resolve();
-                }, 1000);
-              });
-            },
-          }}
-        />
+                  setUpdaing(true);
+                  setUserTableData([...userTableData, newData]);
+                  await axios.put(
+                    `https://jsonplaceholder.typicode.com/users/${rowData.id}`,
+                    newData
+                  );
+                  setUpdaing(false);
+                  setTimeout(resolve, 1000);
+                });
+              },
+            }}
+          />
+          {updating && <h3>Updating......</h3>}
+        </React.Fragment>
       )}
     </div>
   );
